04dacffe63412f62705ab8471ff3476e1dc04ec3
Mohd Abrar Hossain <mhossain69@myseneca.ca>
2024-05-15 18:26:01 -0400

    Lab2f submitted

---
+++ lab2f.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+## Author: Mohd Abrar Hossain
+## Author ID: mhossain69@myseneca.ca
+## Date Created: 2024/05/15
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
