bc3c64f5d400dc5524c9f31702ee19e2115c8915
Mohd Abrar Hossain <mhossain69@myseneca.ca>
2024-05-15 18:25:46 -0400

    Lab2e submitted

---
+++ lab2e.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+## Author: Mohd Abrar Hossain
+## Author ID: mhossain69@myseneca.ca
+## Date Created: 2024/05/15
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
